019d01c4ff9abcff84e5911680d956de98fe21c3
Joshua Davey <josh@joshuadavey.com>
2014-10-28 11:06:33 -0500

    doto

--- lib/clojure/core.clj
+++ lib/clojure/core.clj
@@ -3563,22 +3563,22 @@
 ;     :else (throw (ArgumentError.
 ;                    "with-open only allows Symbols in bindings"))))
 
-; (defmacro doto
-;   "Evaluates x then calls all of the methods and functions with the
-;   value of x supplied at the front of the given arguments.  The forms
-;   are evaluated in order.  Returns x.
+(defmacro doto
+  "Evaluates x then calls all of the methods and functions with the
+  value of x supplied at the front of the given arguments.  The forms
+  are evaluated in order.  Returns x.
 
-;   (doto (new java.util.HashMap) (.put \"a\" 1) (.put \"b\" 2))"
-;   {:added "1.0"}
-;   [x & forms]
-;     (let [gx (gensym)]
-;       `(let [~gx ~x]
-;          ~@(map (fn [f]
-;                   (if (seq? f)
-;                     `(~(first f) ~gx ~@(next f))
-;                     `(~f ~gx)))
-;                 forms)
-;          ~gx)))
+  (doto (.new Hash) (.[]= \"a\" 1) (.[]= \"b\" 2))"
+  {:added "1.0"}
+  [x & forms]
+    (let [gx (gensym)]
+      `(let [~gx ~x]
+         ~@(map (fn [f]
+                  (if (seq? f)
+                    `(~(first f) ~gx ~@(next f))
+                    `(~f ~gx)))
+                forms)
+         ~gx)))
 
 ; (defmacro memfn
 ;   "Expands into code that creates a fn that expects to be passed an
