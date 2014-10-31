4b946710ad021fa3b78f1c1fca35072d178ea21e
Joshua Davey <josh@joshuadavey.com>
2014-10-31 13:18:55 -0500

    defonce

--- lib/clojure/core.clj
+++ lib/clojure/core.clj
@@ -5371,14 +5371,14 @@
   [& filters]
   `(clojure.core/refer '~'clojure.core ~@filters))
 
-; (defmacro defonce
-;   "defs name to have the root value of the expr iff the named var has no root value,
-;   else expr is unevaluated"
-;   {:added "1.0"}
-;   [name expr]
-;   `(let [v# (def ~name)]
-;      (when-not (.hasRoot v#)
-;        (def ~name ~expr))))
+(defmacro defonce
+  "defs name to have the root value of the expr iff the named var has no root value,
+  else expr is unevaluated"
+  {:added "1.0"}
+  [name expr]
+  `(let [v# (def ~name)]
+     (when-not (.root? v#)
+       (def ~name ~expr))))
 
 ; ;;;;;;;;;;; require/use/load, contributed by Stephen C. Gilardi ;;;;;;;;;;;;;;;;;;
 
